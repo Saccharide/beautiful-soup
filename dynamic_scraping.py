0e54b1ec7593d0d972544c0e6fd7c6c7c281c60d
saccharide <rtang@email.wm.edu>
2019-03-26 10:51:20 -0400

    removed venv because this is a bad idea if you are working from multiple device

--- dynamic_scraping.py
+++ dynamic_scraping.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-
-resource = requests.get('https://www.google.com')
-
+resource = requests.get('http://thingsthataresmart.wiki/index.php?title=Category:Unpublished_SmartApps')
 soup = BeautifulSoup(resource.text, 'lxml')
-
 
 # Normal scraping
 container = soup.find('div')
+
+# https://github.com/search?l=Groovy&p=1&q=smartthings&type=Repositories
 print(container)
+hmenu = container.find_all('div','hmenu')
+print(hmenu)
